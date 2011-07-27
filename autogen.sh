f27e17e3fcd98d052a528def3b735377d458ec16
Joshua Wise <jwise@andrew.cmu.edu>
2011-07-27 00:54:05 -0700

    Default to silent rules, to make warnings more obvious.  This now mandates a dependency on automake-1.11; rerun autogen.sh to build.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 set -x
-autoreconf 
 libtoolize -f || exit 1
-aclocal-1.9 || exit 1
+aclocal-1.11 || exit 1
 autoconf || exit 1
 autoheader || exit 1
-automake-1.9 -a || exit 1
+automake-1.11 -a || exit 1
