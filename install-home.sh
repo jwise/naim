a5c3b5c6d102930faccaf02fbad53fbd32eca4c8
Joshua Wise <joshua@joshuawise.com>
2011-09-05 03:46:40 -0700

    Add an install-home.sh to install into your home directory.  Hooray!  No more init.lua!

---
+++ install-home.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Installing OSCAR support into your home directory..."
+
+set -x
+mkdir -p ~/.naim
+touch ~/.naim/userinit.lua
+if ! grep OSCAR ~/.naim/userinit.lua >/dev/null ; then
+	echo 'require"OSCAR"' >> ~/.naim/userinit.lua
+fi
+cp numutil.lua ~/.naim/
+mkdir -p ~/.naim/OSCAR
+cp OSCAR.lua ~/.naim/OSCAR/init.lua
+cp FLAP.lua ~/.naim/OSCAR/FLAP.lua
+cp SNAC.lua ~/.naim/OSCAR/SNAC.lua
+cp TLV.lua ~/.naim/OSCAR/TLV.lua
+set +x
+
+echo "Looks done to me, unless you saw any errors above."
